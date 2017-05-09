f97ac80b09ba0b1cdad05c1a67e2b6ae7eb3c661
Kython Shu <kythonshu@gmail.com>
2017-05-09 11:35:35 +0800

    Initial Commit

---
+++ 画板/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  画板
+//
+//  Created by Kython Shu on 2017/5/9.
+//  Copyright © 2017年 Kython Shu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 画板/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  画板
+//
+//  Created by Kython Shu on 2017/5/9.
+//  Copyright © 2017年 Kython Shu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
